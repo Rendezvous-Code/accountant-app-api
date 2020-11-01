090a740d188745048a3a0fc3590f5c8269fa0097
havocZz <gema71wb@gmail.com>
2020-11-01 23:47:23 +0100

    added models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@rendezvous.rs', password='testpass'):
+    """Create sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -37,3 +43,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_account_str(self):
+        """Test business entity str rep"""
+        account = models.Account.objects.create(
+            name='Agencija racunovodstvo'
+        )
+        self.assertEqual(str(account), account.name)
+
+
